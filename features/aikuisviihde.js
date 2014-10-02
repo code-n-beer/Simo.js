61ba4b357e2f9b32df0e4e9c6a13e5a3dc60b171
J. Honeyhillicus <jmesimak@cs.helsinki.fi>
2014-10-02 22:54:38 +0300

    sproink

--- features/aikuisviihde.js
+++ features/aikuisviihde.js
@@ -1,5 +1,7 @@
 var request = require('request');
 
+var seen = [];
+
 var gonewild = function(client, channel, from, line) {
 	var url = "http://www.reddit.com/r/gonewild/hot.json?sort=new";
 	var response;
@@ -7,8 +9,19 @@ var gonewild = function(client, channel, from, line) {
 	request(url, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    response = JSON.parse(body);
-	    var topPost = response.data.children[1];
+	    var count = 1;
+		var topPost = response.data.children[count];
 	    var topPostImg = topPost.data.url;
+
+	    while (seen.indexOf(topPostImg) !== -1) {
+		    topPost = response.data.children[count];
+		    topPostImg = topPost.data.url;
+		    count++;
+	    }
+
+	    seen.push(topPostImg);
+	    console.log(seen);
+
 	    client.say(channel, topPostImg);
 	  }
 	});
