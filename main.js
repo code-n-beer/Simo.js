52ad7c14ed27144a85239d3cf2a811f79bd21496
tsarpf <tsarpf@gmail.com>
2014-10-14 20:40:49 +0300

    Fixed merge

--- main.js
+++ main.js
@@ -42,46 +42,18 @@ for(var init in inits)
     inits[init](config);
 }
 
+var logger = require('./features/simoOnFire.js').loggingAction;
 
 client.addListener('message', function(from, to, message) {
     //console.log("from: " + from);
     //console.log("to: " + to);
     //console.log("message: " + message);
-    if (message[0] === '!') {
-
-      var wanhat = ["!expl", "!horos", "!lastfm", "!mötö", "!unmötö", "!niksi",
-      "!r", "!uc", "!weather", "!uguu", "!add", "!remove", "!c", "!pizza", "!tweet", "!np"];
-
-      var cmd = message.split(" ")[0];
-      if (commands[cmd] !== undefined || wanhat.indexOf(cmd) !== -1) {
-        function checkIfCmdInFirebase(input) {
-          simoOnFire.child(input).on("value", function(val) {
-            return (val.val() !== null);
-          });
-        }
-
-        function createEntryToFirebase(comm) {
-          var obj = {};
-          obj[comm] = 1;
-          simoOnFire.set(obj);
-        }
-
-        function updateEntryInFirebase(comm) {
-          simoOnFire.child(comm).once("value", function(val) {
-            var obj = {};
-            obj[comm] = val.val()+1;
-            simoOnFire.update(obj);
-          });
-        }
-
-        if (checkIfCmdInFirebase(cmd)) {
-          createEntryToFirebase(cmd);
-        } else {
-          updateEntryInFirebase(cmd);
-        }
-      }
-    } 
-    require('./features/simoOnFire.js').loggingAction(from, to, message, commands);
+    try {
+        logger(from, to, message, commands);
+    }
+    catch(err){
+        console.log("logger crashed: " + err);
+    }
 
     var msg = message.toLowerCase();
     if(msg.indexOf("penis") != -1)
@@ -94,28 +66,35 @@ client.addListener('message', function(from, to, message) {
         to = from;
     }
 
-    var cmd = msg.split(" ")[0];
-    if(commands.hasOwnProperty(cmd))
-    {
-        var functions = commands[cmd];
-        //console.log("functions: " + functions);
-        functions.forEach(function(func) {
-            func(client, to, from, message);
+    try {
+        var cmd = msg.split(" ")[0];
+        if(commands.hasOwnProperty(cmd))
+        {
+            var functions = commands[cmd];
+            //console.log("functions: " + functions);
+            functions.forEach(function(func) {
+                func(client, to, from, message);
+            });
+        }
+        }
+    catch (err) {
+        console.log(err);
+    }
+    try {
+        Object.keys(regexes).forEach(function(key) {
+                var regex = new RegExp(key);
+                if(message.match(regex))
+                {
+                    for(var i = 0; i < regexes[key].length; i++)
+                    {
+                        regexes[key][i](client, to, from, message);
+                    }
+                }
         });
     }
-    Object.keys(regexes).forEach(function(key) {
-        //Object.keys(regexes[obj]).forEach(function (key) {
-            var regex = new RegExp(key);
-            //if(regex.indexOf(message) !== -1)
-            if(message.match(regex))
-            {
-                for(var i = 0; i < regexes[key].length; i++)
-                {
-                    regexes[key][i](client, to, from, message);
-                }
-            }
-        //});
-    });
+    catch(err){
+        console.log(err);
+    }
 });
 
 client.connect();
