894580411f53bfc10fc497b0506973ac301f3ecb
Tsarpf <tsarpf@gmail.com>
2015-06-10 23:11:31 +0300

    Add sandcastle

--- features/sandcastle.js
+++ features/sandcastle.js
@@ -1,33 +1,34 @@
-var Sandbox = require('sandcastle').SandCastle;
+var SandCastle = require('sandcastle').SandCastle;
 //client is used to send stuff
 //channel is needed to send stuff using client, but is also the channel's name where the line came from
 //line is the full message the user sent
 //from is the nick of the user who sent the line
 var run = function(client, channel, from, line){
   //console.log(line); //debug
-  var sbox = new SandCastle();
+  var sbox = new SandCastle({
+    api: __dirname + '/../lib/api.js',
+      memoryLimitMB: 100,
+      timeout: 3000
+  });
 
   line = line.substring(5);
-  var script = sbox.createScript(line);
+  var script = sbox.createScript("exports.main = function() {" + line + "}");
+
   script.on('exit', function(err, output) {
-    var res = output.replace(/(\r\n|\n|\r)/gm,' ');
-    client.say(channel, res);
+    console.log('err: ' + err);
+    console.log('output: ' + output);
+    if(!err) {
+      res = JSON.stringify(output);
+      res = res.replace(/(\r\n|\n|\r)/gm,' ');
+      client.say(channel, res);
+    }
+    else {
+      res = err.toString();
+      client.say(channel, res);
+    }
   });
 
   script.run();
-  /*
-  s.run(line, function(output) {
-    var res = output.result.replace(/(\r\n|\n|\r)/gm,' ');
-    res = res.replace('   ', ' ');
-    res = res.replace('  ', ' ');
-    output.result = res;
-    res = JSON.stringify(output);
-    console.log(res);
-    console.log(output.console);
-    client.say(channel, res); 
-  });
-  */
-
 }
 
 module.exports = {
