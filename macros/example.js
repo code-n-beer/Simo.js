8a937dcf01db67444c032be4ae40bee6be5ca292
Tsarpf <tsarpf@gmail.com>
2015-06-11 13:17:45 +0300

    Just sharing unfinished stuff from before

---
+++ macros/example.js
@@ -0,0 +1,3 @@
+module.exports = {
+  '_test': "exit('test')"
+}
---
+++ macros/index.js
@@ -0,0 +1,19 @@
+var macros = {};
+require("fs").readdirSync("./features/").forEach(function(file) {
+      if(file !== 'index.js'){
+          console.log("file: " + file);
+          files.push(file);
+          var fileMacros = require("./" + file.substring(0,file.length-3));
+
+          for(var macro in fileMacros){
+            if(fileMacros.hasOwnProperty(macro)) {
+              macros[macro] = fileMacros[macro];
+              console.log('%s --> %s', macro, macros[macro]);
+            }
+          }
+      }
+});
+
+module.exports = {
+  macros: macros
+}
