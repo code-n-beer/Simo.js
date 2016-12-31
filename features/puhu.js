7d4541699528f071d8f07e58b7cddf96aedd76ba
Teemu Sarapisto <simobot@vmi90544.contabo.host>
2016-12-31 18:59:02 +0200

    Add !ketÃ

--- features/puhu.js
+++ features/puhu.js
@@ -44,22 +44,62 @@ const querySes = (sesPath, line, callback) => {
     const out = targetLength
       ? lines.map(line => { return {line, dist: Math.abs(line.length - targetLength)}}).reduce((cur, best) => cur.dist < best.dist ? cur : best, {line:'', dist: Number.MAX_SAFE_INTEGER}).line
       : lines[primetext ? 0 : Math.floor(Math.random() * lines.length)]
-    callback(out.trim().replace('\n', ' ').substr(0, 500))
+    callback(out.trim().replace('\n', ' ').substr(0, 500), lines)
   })
 }
 
 const puhu = (client, channel, from, line) => {
-  querySes('nns/cv_0.4/lm_lstm_epoch50.00_1.3930.t7', line, res => client.say(channel, res))
+  querySes('nns/cv_0.4/lm_lstm_epoch50.00_1.3930.t7', line, (res, lines) => {
+  })
 }
 
 const inva = (client, channel, from, line) => {
   querySes('nns/cv_0.1/lm_lstm_epoch50.00_1.7434.t7', line, res => client.say(channel, res + ' :D'))
 }
 
+let ketaNick = 'nobody what the ass?'
+const keta = (client, channel, from, line) =>
+  line.split(' ').length > 1 ? arvaaKeta(client, channel, from, line) : puhuKeta(client, channel, from, line)
+
+
+const puhuKeta = (client, channel, from, line) => {
+  querySes('nns/cv_0.5/lm_lstm_epoch32.03_1.2095.t7', line, (res, lines) => {
+    const sensibleLines = lines.filter(l => ~l.indexOf('\t') && !~l.indexOf('--'))
+    if(!sensibleLines) {
+      client.say(channel, 'Is not of workings. Try again perhaps?')
+      return
+    }
+    const lineArr = sensibleLines.map(l => l.split('\t'))[Math.floor(Math.random() * sensibleLines.length)]
+    ketaNick = lineArr[0].trim()
+    client.say(channel, lineArr[1])
+   })
+}
+
+const arvaaKeta = (client, channel, from, line) => {
+  const random = Math.random()
+  const floored = Math.floor(random*10)
+  let result = ''
+  let answerStr = line.split(' ')[1].toLowerCase() === ketaNick ? 'correct' : 'wrong'
+  if(random < 0.5) {
+    answerStr = (random < 0.2) ? answerStr.toUpperCase() : answerStr
+    result = answerStr + '.'.repeat(floored % 3 + 1)
+  } else if(random < 0.7) {
+    result = 'Your answer was of the following kind: ' + answerStr + '!'.repeat(floored % 4)
+  } else {
+    result = 'Good guess! That was ' + answerStr + '.'.repeat(floored % 3 + 1)
+  }
+  result += ' The correct answer was... ' + ketaNick + '!'.repeat(floored % 3) + '?'.repeat(floored % 1)
+  ketaNick = 'nobody what the ass'
+  client.say(channel, result)
+}
+
+
 module.exports = {
   name: "puhu",
   commands: {
     "!puhu": puhu,
-    "!inva": inva
+    "!inva": inva,
+    "!ketä":keta,
+    "!keta": keta
   }
 }
