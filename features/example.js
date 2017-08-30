6ac3d8307e009e79ade72137f5bc63bee9052253
Teemu Sarapisto <tsarpf@gmail.com>
2017-08-31 00:11:40 +0300

    Fix style for example.js

--- features/example.js
+++ features/example.js
@@ -1,31 +1,30 @@
-
-
 /*
- *   COPY THIS FILE TO CREATE YOUR OWN FEATURE 
- *   DON'T PUT YOUR FEATURE WITHIN THIS FILE
- */
+*   COPY THIS FILE TO CREATE YOUR OWN FEATURE
+*   DON'T PUT YOUR FEATURE WITHIN THIS FILE
+*/
 
 //when placed in the return object, will be run on simojs startup
-var init = function() {
-	
+const init = function() {
+
 }
 
-var count = 0;
+let count = 0
 //client is used to send stuff
 //channel is needed to send stuff using client, but is also the channel's name where the line came from
 //line is the full message the user sent
 //from is the nick of the user who sent the line
-var hello = function(client, channel, from, line){
-    //console.log(line); //debug
-    count++;
-    var ret =  "Feature has been ran " + count + " times.";
-    client.say(channel, ret); 
+// eslint-disable-next-line no-unused-vars
+const hello = function(client, channel, from, line){
+  //console.log(line); //debug
+  count++
+  const ret =  'Feature has been ran ' + count + ' times.'
+  client.say(channel, ret)
 }
 
 module.exports = {
-    name: "test", //not required atm iirc 
-    commands: { 
-       "!test": hello,
-    },
-    init: init
+  name: 'test', //not required atm iirc
+  commands: {
+    '!test': hello,
+  },
+  init: init
 }
