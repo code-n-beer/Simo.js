eb7b5f42060c48bba525df509300d68d7f80eef0
Teemu Sarapisto <tsarpf@gmail.com>
2023-01-05 06:38:49 +0200

    Add old shits

--- ml-simo/server.py
+++ ml-simo/server.py
@@ -26,36 +26,58 @@ if __name__ != '__main__':
 #   print('main, skip models')
 #   generator = None
 
+
+def translate_to_fi(text):
+    path = '/translate'
+    constructed_url = endpoint + path
+    params = {
+	'api-version': '3.0',
+	'from': 'en',
+	'to': ['fi']
+    }
+    constructed_url = endpoint + path
+    headers = {
+	'Ocp-Apim-Subscription-Key': subscription_key,
+	'Ocp-Apim-Subscription-Region': location,
+	'Content-type': 'application/json',
+	'X-ClientTraceId': str(uuid.uuid4())
+    }
+    # You can pass more than one object in body.
+    body = [{
+	'text': text 
+    }]
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    response = request.json()
+    return response[0]['translations'][0]['text']
+
 def write_to_file(input_str):
   uuid = shortuuid.uuid()
 
-  filename = "%s.html" % uuid
+  if "<html>" in input_str.lower():
+    filename = "%s.html" % uuid
+  else:
+    filename = "%s.txt" % uuid
+
   with open('/mlsimo-data/output/' + filename, "w", encoding="utf8") as text_file:
     text_file.write(input_str)
     
   return filename
 
-def wrap_pre(result_str):
-    if "<html>" in result_str.lower():
-        return result_str
-    else:
-        return "<pre>\n" + result_str + "\n</pre>"
-    
-
-def gen(prompt, bad_words, settings):
+def gen(prompt, bad_words, settings, translate=False):
   if settings is None:
     settings = default_settings
-  if settings['max_length'] > 20000:
+  if 'max_length' in settings and settings['max_length'] > 20000:
     settings['max_length'] = 20000
 
-  if bad_words and len(bad_words > 0):
+  if bad_words and len(bad_words) > 0:
     ebun = generator(prompt, **settings,
       bad_words_ids=generator.tokenizer(bad_words)['input_ids'])
   else:
     ebun = generator(prompt, **settings)
 
   output = ebun[0]['generated_text']
-  output = wrap_pre(output)
+  if translate:
+      output = translate_to_fi(output)
   filename = write_to_file(output)
 
   return filename, output
@@ -70,11 +92,12 @@ def index():
 @app.route('/gpt', methods=['POST'])
 def add_message():
   content = request.json
-  prompt = content['prompt']
+  prompt = content['prompt'].replace("|","\n")
   bad_words = content['bad_words'] if 'bad_words' in content else None
   settings = content['settings'] if 'settings' in content else None
+  translate = True if 'translate' in content else False
 
-  filename, output = gen(prompt, bad_words, settings)
+  filename, output = gen(prompt, bad_words, settings, translate)
 
   return jsonify({'filename': filename, 'output': output})
 
