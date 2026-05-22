31648c3d69c3c24d9f3a899678d3d5149fe6dc0d
Jere <jere89@gmail.com>
2026-05-23 01:28:47 +0300

    Fix !cpu: include cpu time in output

--- features/usage.js
+++ features/usage.js
@@ -3,7 +3,8 @@ var sys_usage = function(client, channel, from, line) {
     const cpu = process.cpuUsage();
     const heapMB = (mem.heapUsed / 1024 / 1024).toFixed(1);
     const rssMB = (mem.rss / 1024 / 1024).toFixed(1);
-    client.say(channel, `Simo memory: ${heapMB}MB heap, ${rssMB}MB rss`);
+    const cpuSecs = ((cpu.user + cpu.system) / 1e6).toFixed(1);
+    client.say(channel, `Simo memory: ${heapMB}MB heap, ${rssMB}MB rss, cpu: ${cpuSecs}s`);
 }
 
 module.exports = {
